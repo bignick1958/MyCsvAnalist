3acde81b77cfd852e08953bb6aaf2d65b8a93bce
bignick1958 <bignick58@gmail.com>
2022-09-21 07:58:19 +0300

    Update CsvAnalist.py
    Создание меток и полей

--- CsvAnalist.py
+++ CsvAnalist.py
@@ -1,9 +1,28 @@
-import tkinter
-
 import tkinter as tk
 
-window=tk.Tk()
+# creating MAIN window
+window = tk.Tk()
 window.geometry('600x600')
 window.title('Анализатор CSV-файлов ')
+
+# creating output Lables
+lable_00= tk.Label(text='Файл:')
+lable_00.grid(row=0, column=0, padx=10, pady=10, sticky='e')
+
+lable_01= tk.Label(text='')
+lable_01.grid(row=0, column=1, sticky='w')
+
+lable_10= tk.Label(text='Строк:')
+lable_10.grid(row=1, column=0, padx=10, pady=10, sticky='e')
+
+lable_11= tk.Label(text='')
+lable_11.grid(row=1, column=1, sticky='w')
+
+lable_20= tk.Label(text='Столбцов:')
+lable_20.grid(row=2, column=0, padx=10, pady=10, sticky='e')
+
+lable_21= tk.Label(text='')
+lable_21.grid(row=2, column=1, sticky='w')
+
 
 window.mainloop()
